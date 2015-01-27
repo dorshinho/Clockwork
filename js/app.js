367cd415dc25388c741ef7542efb6397567ef1f8
Dorsh <dorshdeans@gmail.com>
2015-01-26 23:56:36 -0500

    updating gh-pages

--- js/app.js
+++ js/app.js
@@ -28,6 +28,11 @@ setInterval(function(){
     h1s.forEach(function(el){
         el.innerHTML = parts.join(":")
     })
+    //Write a function that will make sure any number less than "10" has a zero before it.
+    // for (var i = 0; i < Things.length; i++) {
+    //     Things[i]
+    // };
+
          document.getElementById("backdrop").style.backgroundColor = "rgb(" + Math.floor((d.getHours() / .0705)) + ", " + Math.floor((d.getMinutes() / .387)) + ", " + Math.floor((d.getSeconds() / .255)) + ")";
 
 }, 1*1000)
